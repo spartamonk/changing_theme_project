98366d24d2b33413cad9a3d369401905351a8791
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2021-06-29 17:48:22 -0500

    project refactored

--- src/App.js
+++ src/App.js
@@ -2,43 +2,44 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
-const getLocalStorage = () => {
-  let theme = 'light-theme'
+const getLocalStorage =()=> {
+  let theme = 'light-theme';
   if(localStorage.getItem('theme')) {
     theme = localStorage.getItem('theme')
   }
   return theme
 }
+
 function App() {
   const [theme, setTheme] = useState(getLocalStorage())
 
   useEffect(()=> {
-    document.documentElement.className= theme;
-    localStorage.setItem('theme', theme)
-  }, [theme])
+    document.documentElement.className = theme;
+    localStorage.setItem('theme',theme)
+  },[theme])
 
-  const toggleButton =()=> {
+  const handleToggle =()=> {
     if(theme === 'light-theme') {
       setTheme('dark-theme')
     } else {
       setTheme('light-theme')
     }
   }
+
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn" onClick={toggleButton}>toggle</button>
+          <button className="btn" onClick={handleToggle}>toggle</button>
         </div>
       </nav>
       <section className="articles">
         {
-          data.map((item)=> {
-            const {id} = item
-            return (
-              <Article key={id} {...item} />
-            )
+          data.map(item=> {
+            const {id}= item
+            return <Article key={id} {...item} />
           })
         }
       </section>
--- src/Article.js
+++ src/Article.js
@@ -1,21 +1,17 @@
 import React from 'react'
 import moment from 'moment'
-const Article = ({ title, date, length, snippet }) => {
+const Article = ({title, date, length, snippet}) => {
   return (
     <article className='post'>
       <h2>{title}</h2>
-      <div className="post-info">
-        <span>
-          {
-            moment(date).format('dddd Do, YYYY')
-          }
-        </span>
+      <div className='post-info'>
+        <span>{moment(date).format('dddd Do, YYYY')}</span>
         <span>
           {length} min read
         </span>
       </div>
       <p>{snippet}</p>
-  </article>
+    </article>
   )
 }
 
